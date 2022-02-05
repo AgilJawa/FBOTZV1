27ae1518aca15c1f954a9277af37bb3d85094148
Agil0fc1 <agildeveloper@outlook.co.id>
2022-02-05 22:32:55 +0700

    FBOTZV1

---
+++ FbotzSynyster/donasi.js
@@ -0,0 +1,18 @@
+const donasi = (prefix, donasi, botname) => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          DONASI BRO?  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱❉ *DONASI YOK* ❉⊰━━✿
+┃   
+┣━⊱ *GOPAY*
+┣⊱ -
+┣━⊱ *DANA*
+┣⊱ -
+┣━⊱ *PULSA*
+┣⊱ 081394567319
+┃
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
